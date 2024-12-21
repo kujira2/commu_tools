33ff16de48fa893fc4a082734fdc31461f754ef8
kujira2 <54442474+kujira2@users.noreply.github.com>
2024-12-21 11:06:54 +0900

    Update flow-chart.js

--- flow-chart.js
+++ flow-chart.js
@@ -42,6 +42,9 @@ export class FlowChartManager {
                             text: '出力モニター（20%F.S.刻みで10s間）'
                         }
                     },
+                        legend: {
+                            display: false  // 凡例を非表示に設定
+                        }
                     scales: {
                         x: {
                             type: 'linear',
